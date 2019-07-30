ce0e3d71cff6228a9af452500b42be120cc06371
mohayemin <mohayeminul@iut-dhaka.edu>
2019-07-30 16:12:23 +0600

    initialize angular project

---
+++ DevConsole/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ia-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'iutapi-devconsole';
+  teamname = 'codeblue';
+}
