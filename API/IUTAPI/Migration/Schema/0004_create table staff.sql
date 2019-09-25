f51a6f309f1083db64ab9335022e818928c4e5a9
Ela_Razia_Zaman <53617549+Ela-Zaman@users.noreply.github.com>
2019-09-26 02:20:15 +0600

    IAPI-6 Staff Api done

---
+++ API/IUTAPI/Migration/Schema/0004_create table staff.sql
@@ -0,0 +1,7 @@
+﻿Create Table Staff(
+staffid text primary key,
+name text,
+designation text,
+MobileNumber text,
+workarea text,
+Gender text);
---
+++ API/IUTAPI/Models/Staff.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IUTAPI.Models
+{
+    [Table("staff")]
+    public class Staff
+    {
+
+            [Column("staffid")]
+            [Key]
+
+            public string StaffId { get; set; }
+            [Column("name")]
+            public string Name { get; set; }
+            [Column("designation")]
+            public string Designation { get; set; }
+            [Column("mobilenumber")]
+            public string MobileNumber { get; set; }
+        [Column("workarea")]
+        public string WorkArea { get; set; }
+
+        [Column("gender")]
+            public string Gender { get; set; }
+        }
+    }
+
