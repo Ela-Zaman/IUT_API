100c11cb514064690601e815d55fae1a56151a8f
MahirAshrafEmad <MahirAshrafEmad@localhost>
2019-11-12 19:42:16 +0600

    Updated 'Login' appmodule prob before firebase deployment

--- FinalDevConsole/src/app/app.module.ts
+++ FinalDevConsole/src/app/app.module.ts
@@ -7,6 +7,20 @@ import { AdminComponent } from './admin/admin.component';
 import { FormsModule }          from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
+const config = {
+  apiKey: "AIzaSyC57JYiLXcGIRFBDO__nq6iMFQQpakyFuQ",
+  authDomain: "login-iut-7969d.firebaseapp.com",
+  databaseURL: "https://login-iut-7969d.firebaseio.com",
+  projectId: "login-iut-7969d",
+  storageBucket: "",
+  messagingSenderId: "962072782139",
+  appId: "1:962072782139:web:216d279b0e494241cd431e"
+}
+
 
 const appRoutes: Routes = [
   { path: 'welcome', component: WelcomeComponent },
@@ -19,10 +33,14 @@ const appRoutes: Routes = [
     AppComponent,
     WelcomeComponent,
     HomeComponent,
-    AdminComponent],
+    AdminComponent
+  ],
   imports: [
     BrowserModule,
     FormsModule,
+    AngularFireModule.initializeApp(config),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
     RouterModule.forRoot(
       appRoutes,
       { enableTracing: true } // <-- debugging purposes only
